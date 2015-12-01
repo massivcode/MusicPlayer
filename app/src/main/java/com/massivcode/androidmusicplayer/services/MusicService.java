1fcfae94ae46a72bd6c65b2af34398def852ecf1
Junsuk Oh <a811219@gmail.com>
2015-12-01 21:49:29 +0900

    패키지 상호참조 제거 : Activity, Service 직접 호출 explicit intent -> implicit intent 로 수정

--- app/src/main/java/com/massivcode/androidmusicplayer/services/MusicService.java
+++ app/src/main/java/com/massivcode/androidmusicplayer/services/MusicService.java
@@ -50,6 +50,8 @@ import de.greenrobot.event.EventBus;
 public class MusicService extends Service implements MediaPlayer.OnCompletionListener {
     private static final String TAG = MusicService.class.getSimpleName();
 
+    public static final String ACTION_NAME = "com.massivcode.androidmusicplayer.MusicService";
+
     public static final String ACTION_PLAY = "ACTION_PLAY";
     public static final String ACTION_PLAY_NEXT = "ACTION_PLAY_NEXT";
     public static final String ACTION_PLAY_PREVIOUS = "ACTION_PLAY_PREVIOUS";
@@ -713,7 +715,7 @@ public class MusicService extends Service implements MediaPlayer.OnCompletionLis
 
         // Notification 터치 했을 때 실행할 PendingIntent 지정
         // =========================================================================================
-        Intent activityStartIntent = new Intent(getApplicationContext(), MainActivity.class);
+        Intent activityStartIntent = new Intent(MainActivity.ACTION_NAME);
         activityStartIntent.putExtra("restore", getCurrentInfo());
         PendingIntent activityStartPendingIntent = PendingIntent.getActivity(getApplicationContext(),
                 1,
